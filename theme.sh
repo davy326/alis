1ffeb5e231be086dc831d82ee9979ed2abf5c2f5
davy326 <70665197+davy326@users.noreply.github.com>
2021-10-08 23:11:27 -0600

    Add files via upload

---
+++ theme.sh
@@ -0,0 +1,12 @@
+sudo mv flat-mountain.jpg /usr/share/backgrounds
+
+sudo git clone https://github.com/vinceliuice/Qogir-theme.git /home/davy/qogir
+cd /home/davy/qogir
+./install.sh
+
+git clone https://github.com/vinceliuice/Qogir-icon-theme.git /home/davy/qogir-icon
+cd /home/davy/qogir
+./install.sh
+
+
+
